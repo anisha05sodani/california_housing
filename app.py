5dceeaca8d1744713a7e317483ffcc53d3122d97
anisha05sodani <anishasodani05@gmail.com>
2025-03-10 02:12:35 +0530

    webpage+Procfile

--- app.py
+++ app.py
@@ -24,5 +24,15 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    print(output)
+    return render_template("home.html",prediction_text="Price prediction value is {:.2f}".format(output))
+    
+
 if __name__=="__main__":
     app.run(debug=True,port=8081)
